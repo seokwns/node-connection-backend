5936dde8337c35d6a16955ef349ae8e429f7c839
seokwns <seokjun0915@icloud.com>
2024-07-23 14:28:19 +0900

    feat: add application script
    - 네트워크, 프론트엔드, 백엔드 어플리케이션 시작 및 종료 스크립트 추가

--- node-connection.sh
+++ node-connection.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-function start_network() {
+function start_application() {
     echo "Starting node-connection network..."
     ./network/node-connection-network/network.sh up -ca
+
+    # TODO: 백엔드 서버 시작
+    
+    # TODO: 프론트엔드 서버 시작
 }
 
 # Function to stop the network
-function stop_network() {
+function stop_application() {
     echo "Stopping node-connection network..."
     ./network/node-connection-network/network.sh down
+
+    # TODO: 백엔드 서버 중지
+
+    # TODO: 프론트엔드 서버 중지
 }
 
 # Check the first argument passed to the script
-if [ "$1" == "up" ]; then
-    start_network
-elif [ "$1" == "down" ]; then
-    stop_network
+if [ "$1" == "start" ]; then
+    start_application
+  
+elif [ "$1" == "stop" ]; then
+    stop_application
 else
-    echo "Invalid argument. Use 'up' to start the network and 'down' to stop the network."
+    echo "Invalid argument. Use 'start' to start the application and 'stop' to stop the application."
     exit 1
 fi
