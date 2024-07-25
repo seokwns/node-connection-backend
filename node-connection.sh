a12e7502455db75a15052c7e272df4cfbc589f72
seokwns <seokjun0915@icloud.com>
2024-07-25 14:33:16 +0900

    feat: add hyperledger indy install script
    - 하이퍼레저 인디 설치 스크립트 추가
    - 터미널 출력 시 색깔 추가

--- node-connection.sh
+++ node-connection.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-function setup_hyperledger() {
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+function setup_hyperledger_fabric() {
     if [ ! -d "./network/bin" ] || [ ! -d "./network/builders" ] || [ ! -d "./network/config" ]; then
-        echo "Required folders not found in ./network/. Proceeding with setup."
+        echo -e "${YELLOW}Required folders not found in ./network/. Proceeding with setup.${NC}"
 
         curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh
         chmod +x install-fabric.sh
@@ -18,31 +24,55 @@ function setup_hyperledger() {
         rm -rf fabric-samples
         rm -f install-fabric.sh
 
-        echo "Hyperledger Fabric setup complete."
+        echo -e "${GREEN}Hyperledger Fabric setup complete.${NC}"
     else
-        echo "The required folders already exist in ./network/. No actions needed."
+        echo -e "${RED}The required folders already exist in ./network/. No actions needed.${NC}"
+    fi
+}
+
+function setup_hyperledger_indy() {
+    if [ ! -d "$HOME/indy-sdk" ]; then
+        echo -e "${YELLOW}The indy-sdk folder does not exist. Proceeding with cloning.${NC}"
+
+        git clone https://github.com/hyperledger/indy-sdk.git
+
+        cd indy-sdk
+
+        docker build -f ci/indy-pool.dockerfile -t indy_pool .
+        docker run -itd -p 9701-9708:9701-9708 indy_pool
+
+        echo -e "${GREEN}indy-sdk installation and Docker container setup are complete.${NC}"
+    else
+        echo -e "${RED}The indy-sdk folder already exists. No actions will be performed.${NC}"
     fi
 }
 
 function start_application() {
-    setup_hyperledger
+    echo -e "${YELLOW}Starting node-connection application...${NC}"
 
-    echo "Starting node-connection network..."
+    echo -e "${YELLOW}Checking Hyperledger installation...${NC}"
+    setup_hyperledger_fabric
+    setup_hyperledger_indy
+
+    echo -e "${YELLOW}Starting node-connection network...${NC}"
     ./network/node-connection-network/network.sh up -ca
 
     # TODO: 백엔드 서버 시작
     
     # TODO: 프론트엔드 서버 시작
+    echo -e "${GREEN}Starting node-connection application complete.${NC}"
 }
 
-# Function to stop the network
+# Function to stop the networkw
 function stop_application() {
-    echo "Stopping node-connection network..."
+    echo -e "${YELLOW}Stopping node-connection network...${NC}"
     ./network/node-connection-network/network.sh down
 
     # TODO: 백엔드 서버 중지
 
     # TODO: 프론트엔드 서버 중지
+
+    echo -e "${GREEN}Stopping node-connection application complete.${NC}"
 }
 
 # Check the first argument passed to the script
@@ -52,6 +82,6 @@ if [ "$1" == "start" ]; then
 elif [ "$1" == "stop" ]; then
     stop_application
 else
-    echo "Invalid argument. Use 'start' to start the application and 'stop' to stop the application."
+    echo -e "${RED}Invalid argument. Use 'start' to start the application and 'stop' to stop the application.${NC}"
     exit 1
 fi
