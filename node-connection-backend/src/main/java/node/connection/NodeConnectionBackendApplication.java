1398d1479c17d1922da0cf427d7e9e405074c99a
seokwns <seokjun0915@icloud.com>
2024-07-29 21:27:23 +0900

    init: spring boot backend server configuration
    - 스프링 부트 백엔드 생성

---
+++ node-connection-backend/src/main/java/node/connection/NodeConnectionBackendApplication.java
@@ -0,0 +1,13 @@
+package node.connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeConnectionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeConnectionBackendApplication.class, args);
+    }
+
+}
