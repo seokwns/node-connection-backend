5539fab0c5ed2daf3964142742bb0972b15d79f3
seokwns <seokjun0915@icloud.com>
2024-07-30 16:59:34 +0900

    feat: exception and response module implement
    - 응답 및 오류 관련 파일 추가

--- node-connection-backend/src/main/java/node/connection/NodeConnectionBackendApplication.java
+++ node-connection-backend/src/main/java/node/connection/NodeConnectionBackendApplication.java
@@ -1,9 +1,13 @@
 package node.connection;
 
+import node.connection._core.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:properties/env.yaml",
+                factory = EnvConfig.class)
 public class NodeConnectionBackendApplication {
 
     public static void main(String[] args) {
---
+++ node-connection-backend/src/main/java/node/connection/_core/response/ErrorData.java
@@ -0,0 +1,7 @@
+package node.connection._core.response;
+
+public record ErrorData(
+        int code,
+        String message
+) {
+}
