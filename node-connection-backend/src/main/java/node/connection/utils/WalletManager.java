37b43a85f92411562e31cc0a087053ffa2a0bc42
seokwns <seokjun0915@icloud.com>
2024-07-31 09:39:20 +0900

    feat: implement wallet config generator
    - 지갑 설정을 생성하는 클래스 추출

--- node-connection-backend/src/main/java/node/connection/utils/WalletManager.java
+++ node-connection-backend/src/main/java/node/connection/utils/WalletManager.java
@@ -1,34 +1,31 @@
 package node.connection.utils;
 
-import lombok.RequiredArgsConstructor;
 import node.connection._core.exception.ExceptionStatus;
 import node.connection._core.exception.server.ServerException;
+import node.connection.wallet.WalletConfigBuilder;
 import org.hyperledger.indy.sdk.IndyException;
 import org.hyperledger.indy.sdk.pool.Pool;
 import org.hyperledger.indy.sdk.wallet.Wallet;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutionException;
 
-@RequiredArgsConstructor
 @Component
 public class WalletManager {
     static Logger log = LoggerFactory.getLogger(WalletManager.class);
 
     private final Pool pool;
 
-    @Value("${hyperledger.indy.wallet.user-wallet-storage}")
-    private String userWalletStorage;
+    private final WalletConfigBuilder walletConfigBuilder;
 
-    @Value(("${hyperledger.indy.wallet.court-wallet-storage}"))
-    private String courtWalletStorage;
-
-    public WalletManager() throws Exception {
+    public WalletManager(
+            @Autowired WalletConfigBuilder walletConfigBuilder
+    ) throws Exception {
         this.pool = createIndyConnectionPool();
+        this.walletConfigBuilder = walletConfigBuilder;
     }
 
     private Pool createIndyConnectionPool() throws IndyException, ExecutionException, InterruptedException {
@@ -50,7 +47,7 @@ public class WalletManager {
     }
 
     public Wallet createUserWallet(String phoneNumber, String password) {
-        String userConfig = createUserWalletConfig(phoneNumber);
+        String userConfig = this.walletConfigBuilder.getUserWalletConfig(phoneNumber);
         Wallet wallet = createWallet(userConfig, password);
 
         log.info("일반 유저 지갑 생성 완료 | " + phoneNumber);
@@ -58,7 +55,7 @@ public class WalletManager {
     }
 
     public Wallet createCourtWallet(String court, String department, String location, String password) {
-        String courtConfig = createCourtWalletConfig(court, department, location);
+        String courtConfig = this.walletConfigBuilder.getCourtWalletConfig(court, department, location);
         Wallet wallet = createWallet(courtConfig, password);
 
         log.info("등기소 지갑 생성 완료 | " + court + " | " + department + " | " + location);
@@ -66,7 +63,7 @@ public class WalletManager {
     }
 
     private Wallet createWallet(String config, String password) {
-        String credential = createWalletCredential(password);
+        String credential = this.walletConfigBuilder.getWalletCredential(password);
 
         try {
             Wallet.createWallet(config, credential).get();
@@ -78,52 +75,25 @@ public class WalletManager {
     }
 
     public Wallet openUserWallet(String phoneNumber, String password) {
-        String config = createUserWalletConfig(phoneNumber);
+        String config = this.walletConfigBuilder.getUserWalletConfig(phoneNumber);
         return this.openWallet(config, password);
     }
 
     public Wallet openCourtWallet(String court, String department, String location, String password) {
-        String config = createCourtWalletConfig(court, department, location);
+        String config = this.walletConfigBuilder.getCourtWalletConfig(court, department, location);
         return this.openWallet(config, password);
     }
 
     private Wallet openWallet(String config, String password) {
-        String credential = createWalletCredential(password);
+        String credential = this.walletConfigBuilder.getWalletCredential(password);
 
         try {
             Wallet wallet = Wallet.openWallet(config, credential).get();
             log.info("지갑 오픈 완료 | " + config);
+            return wallet;
         } catch (InterruptedException | ExecutionException | IndyException e) {
             e.printStackTrace();
             throw new ServerException(ExceptionStatus.WALLET_OPEN_ERROR);
         }
-    }
-
-    private String createUserWalletConfig(String phoneNumber) {
-        JSONObject storageConfig = new JSONObject().put("path", this.userWalletStorage);
-
-        return new JSONObject()
-                .put("id", phoneNumber)
-                .put("storage_config", storageConfig)
-                .toString();
-    }
-
-    private String createCourtWalletConfig(String court, String department, String location) {
-        JSONObject storageConfig = new JSONObject().put("path", this.courtWalletStorage);
-
-        return new JSONObject()
-                .put("id", this.getCourtId(court, department, location))
-                .put("storage_config", storageConfig)
-                .toString();
-    }
-
-    private String createWalletCredential(String password) {
-        return new JSONObject()
-                .put("key", password)
-                .toString();
-    }
-
-    private String getCourtId(String court, String department, String location) {
-        return court + "_" + department + "_" + location;
     }
 }
