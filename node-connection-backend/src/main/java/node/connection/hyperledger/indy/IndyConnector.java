7a481ce06c0a0a7a71f916942884f8b6ac26eb81
seokwns <seokjun0915@icloud.com>
2024-08-25 16:54:08 +0900

    refactor: change court wallet property name
    - 등기소 지갑 프로퍼티 이름 변경

--- node-connection-backend/src/main/java/node/connection/hyperledger/indy/IndyConnector.java
+++ node-connection-backend/src/main/java/node/connection/hyperledger/indy/IndyConnector.java
@@ -1,10 +1,15 @@
 package node.connection.hyperledger.indy;
 
 import lombok.extern.slf4j.Slf4j;
+import node.connection._core.exception.ExceptionStatus;
+import node.connection._core.exception.server.ServerException;
+import org.hyperledger.indy.sdk.IndyException;
 import org.hyperledger.indy.sdk.pool.Pool;
 import org.hyperledger.indy.sdk.wallet.Wallet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
 
 @Component
 @Slf4j
@@ -29,10 +34,10 @@ public class IndyConnector {
         log.info("일반 유저 지갑 생성 완료 | " + phoneNumber);
     }
 
-    public void createCourtWallet(String court, String department, String location, String password) {
-        String courtConfig = this.walletConfigFactory.getCourtWalletConfig(court, department, location);
+    public void createCourtWallet(String court, String support, String office, String password) {
+        String courtConfig = this.walletConfigFactory.getCourtWalletConfig(court, support, office);
         this.indyUtils.createWallet(courtConfig, password);
-        log.info("등기소 지갑 생성 완료 | " + court + " | " + department + " | " + location);
+        log.info("등기소 지갑 생성 완료 | " + court + " | " + support + " | " + office);
     }
 
     public Wallet openUserWallet(String phoneNumber, String password) {
@@ -43,5 +48,14 @@ public class IndyConnector {
     public Wallet openCourtWallet(String court, String department, String location, String password) {
         String config = this.walletConfigFactory.getCourtWalletConfig(court, department, location);
         return this.indyUtils.openWallet(config, password);
+    }
+
+    public void closeWallet(Wallet wallet) {
+        try {
+            wallet.close();
+        } catch (ExecutionException | InterruptedException | IndyException e) {
+            e.printStackTrace();
+            throw new ServerException(ExceptionStatus.WALLET_CLOSE_ERROR);
+        }
     }
 }
