cf0c4b210721bccb0f197fda15d48c2b3a94653b
teriyakki-jin <jyeg3485@gmail.com>
2024-08-27 21:29:05 +0900

    feat: jwt decode

---
+++ node-connection-backend/src/main/java/node/connection/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package node.connection.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join" , "/api/decode-jwe" ).permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+
+                        .anyRequest().authenticated());
+
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
---
+++ node-connection-backend/src/main/java/node/connection/controller/AuthController.java
@@ -0,0 +1,29 @@
+package node.connection.controller;
+
+import node.connection.jwt.JweDecoder;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+
+    private static final String SECRET = "zm4BqNdeg5dz8sfRuuVbsxdJqXas2b5gse6JTT70hns=";
+    private static final String SALT = "";
+
+    @PostMapping("/decode-jwe")
+    public String decodeJweToken(@RequestBody String jweToken) {
+        try {
+            // Derive the encryption key
+            KeyParameter encryptionKey = JweDecoder.getDerivedEncryptionKey(SECRET, SALT);
+
+            // Decode the JWE token
+            return JweDecoder.decodeJwe(jweToken, encryptionKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error decoding token: " + e.getMessage();
+        }
+    }
+}
