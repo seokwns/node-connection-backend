fb18bae1e195eb994a7a9bf88e36ce5b02f49058
seokwns <seokjun0915@icloud.com>
2024-08-28 21:00:45 +0900

    refactor: class name, field name and etc
    - jwt 코드 병합 내용 정리

--- node-connection-backend/src/main/java/node/connection/_core/security/JweDecoder.java
+++ node-connection-backend/src/main/java/node/connection/_core/security/JweDecoder.java
@@ -1,23 +1,42 @@
 package node.connection.jwt;
 
 
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWEObject;
+import com.nimbusds.jose.Payload;
 import com.nimbusds.jose.crypto.DirectDecrypter;
+import lombok.extern.slf4j.Slf4j;
+import node.connection._core.exception.ExceptionStatus;
+import node.connection._core.exception.server.ServerException;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.generators.HKDFBytesGenerator;
 import org.bouncycastle.crypto.params.HKDFParameters;
 import org.bouncycastle.crypto.params.KeyParameter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
 
 @Component
+@Slf4j
 public class JweDecoder {
 
     private static final String INFO_PREFIX = "NextAuth.js Generated Encryption Key";
 
+    @Value("${security.jwe.secret}")
+    private String secret;
+
+    @Value("${security.jwe.salt}")
+    private String salt;
+
+    public String decode(String token) {
+        KeyParameter parameter = getDerivedEncryptionKey(secret, salt);
+        return decodeJwe(token, parameter);
+    }
+
     // HKDF를 사용하여 키를 유도하는 메서드
-    public static byte[] hkdf(byte[] ikm, byte[] salt, byte[] info, int length) throws Exception {
+    public byte[] hkdf(byte[] ikm, byte[] salt, byte[] info, int length) {
         // HKDF 초기화
         HKDFBytesGenerator hkdf = new HKDFBytesGenerator(new SHA256Digest());
         HKDFParameters params = new HKDFParameters(ikm, salt, info);
@@ -29,7 +48,7 @@ public class JweDecoder {
     }
 
     // JavaScript의 getDerivedEncryptionKey에 해당하는 메서드
-    public static KeyParameter getDerivedEncryptionKey(String keyMaterial, String salt) throws Exception {
+    public KeyParameter getDerivedEncryptionKey(String keyMaterial, String salt) {
         byte[] ikm = keyMaterial.getBytes(StandardCharsets.UTF_8);
         byte[] saltBytes = salt.getBytes(StandardCharsets.UTF_8);
         byte[] info = (INFO_PREFIX + (salt.isEmpty() ? "" : " (" + salt + ")")).getBytes(StandardCharsets.UTF_8);
@@ -38,10 +57,21 @@ public class JweDecoder {
     }
 
     // JWE 디코딩 메서드
-    public static String decodeJwe(String jweString, KeyParameter encryptionKey) throws Exception {
-        JWEObject jweObject = JWEObject.parse(jweString);
-        DirectDecrypter decrypter = new DirectDecrypter(encryptionKey.getKey());
-        jweObject.decrypt(decrypter);
-        return jweObject.getPayload().toString();
+    public String decodeJwe(String jweString, KeyParameter encryptionKey) {
+        JWEObject jweObject = null;
+        try {
+            jweObject = JWEObject.parse(jweString);
+
+            DirectDecrypter decrypter = new DirectDecrypter(encryptionKey.getKey());
+            jweObject.decrypt(decrypter);
+
+            Payload payload = jweObject.getPayload();
+            log.info("Jwe decoded: " + payload.toString());
+
+            return payload.toString();
+        } catch (ParseException | JOSEException e) {
+            e.printStackTrace();
+            throw new ServerException(ExceptionStatus.JWT_DECODE_ERROR);
+        }
     }
 }
--- node-connection-backend/src/main/java/node/connection/entity/UserAccount.java
+++ node-connection-backend/src/main/java/node/connection/entity/UserAccount.java
@@ -1,40 +1,73 @@
 package node.connection.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
-import node.connection.entity.constant.Role;
+import lombok.NoArgsConstructor;
+import node.connection.hyperledger.fabric.Client;
+import node.connection.hyperledger.fabric.ca.Registrar;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Table(name = "user_account_tb")
-public class UserAccount {
+@NoArgsConstructor
+public class FabricRegister {
     @Id
-    private String phoneNumber;
-
-    @Column
     private String name;
 
     @Column
-    private String email;
+    private String mspId;
 
     @Column
-    private String residentNumber;
+    private String secret;
 
-    @Column(nullable = false)
-    private String password;
+    @Column(columnDefinition = "VARCHAR(2000)")
+    private String enrollment;
 
-    @Column(length = 30, nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    @Column(nullable = false)
+    @Column
     private LocalDateTime createdAt;
 
     @Column
     private LocalDateTime updatedAt;
 
-    @Column(name = "is_active")
-    private Boolean isActive;
+    @Builder
+    public FabricRegister(String name, String mspId, String secret, String enrollment, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.name = name;
+        this.mspId = mspId;
+        this.secret = secret;
+        this.enrollment = enrollment;
+        this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
+        this.updatedAt = updatedAt == null ? LocalDateTime.now() : updatedAt;
+    }
+
+    public static FabricRegister of(String name, String mspId, String secret, String enrollment) {
+        return FabricRegister.builder()
+                .name(name)
+                .mspId(mspId)
+                .secret(secret)
+                .enrollment(enrollment)
+                .build();
+    }
+
+    public static FabricRegister of(Registrar registrar, String secret, String enrollment) {
+        return FabricRegister.builder()
+                .name(registrar.getName())
+                .mspId(registrar.getMspId())
+                .secret(secret)
+                .enrollment(enrollment)
+                .build();
+    }
+
+    public static FabricRegister of(Client client, String secret, String enrollment) {
+        return FabricRegister.builder()
+                .name(client.getName())
+                .mspId(client.getMspId())
+                .secret(secret)
+                .enrollment(enrollment)
+                .build();
+    }
 }
