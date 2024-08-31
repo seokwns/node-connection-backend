b0cc9c8dafa5bbc74ac36632b1d91bbd3fa8ef5c
seokwns <seokjun0915@icloud.com>
2024-08-31 15:25:02 +0900

    feat: change court request dto interface
    - 등기소 관련 request dto에서 공통된 court, support, office 컬럼을 CourtRequest 클래스에서 구현 후 extend하여 추가 요청을 구현하도록 수정
    - 공통 함수인 getCourtId 함수 구현

--- node-connection-backend/src/main/java/node/connection/dto/court/request/AddCourtMemberRequest.java
+++ node-connection-backend/src/main/java/node/connection/dto/court/request/AddCourtMemberRequest.java
@@ -1,9 +1,20 @@
 package node.connection.dto.court.request;
 
-public record AddCourtMemberRequest(
-        String court,
-        String support,
-        String office,
-        String memberId
-) {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class AddCourtMemberRequest extends CourtRequest {
+    private final String memberId;
+
+    @JsonCreator
+    public AddCourtMemberRequest(
+            @JsonProperty("court") String court,
+            @JsonProperty("support") String support,
+            @JsonProperty("office") String office,
+            @JsonProperty("memberId") String memberId) {
+        super(court, support, office);
+        this.memberId = memberId;
+    }
 }
--- node-connection-backend/src/main/java/node/connection/dto/court/request/CourtCreateRequest.java
+++ node-connection-backend/src/main/java/node/connection/dto/court/request/CourtCreateRequest.java
@@ -1,15 +1,34 @@
 package node.connection.dto.court.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
 import java.util.List;
 
-public record CourtCreateRequest(
-        String court,
-        String support,
-        String office,
-        String phoneNumber,
-        String address,
-        String faxNumber,
-        List<String> jurisdictions,
-        String walletPassword
-) {
+@Getter
+public class CourtCreateRequest extends CourtRequest {
+    private final String phoneNumber;
+    private final String address;
+    private final String faxNumber;
+    private final List<String> jurisdictions;
+    private final String walletPassword;
+
+    @JsonCreator
+    public CourtCreateRequest(
+            @JsonProperty("court") String court,
+            @JsonProperty("support") String support,
+            @JsonProperty("office") String office,
+            @JsonProperty("phoneNumber") String phoneNumber,
+            @JsonProperty("address") String address,
+            @JsonProperty("faxNumber") String faxNumber,
+            @JsonProperty("jurisdictions") List<String> jurisdictions,
+            @JsonProperty("walletPassword") String walletPassword) {
+        super(court, support, office);
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.faxNumber = faxNumber;
+        this.jurisdictions = jurisdictions;
+        this.walletPassword = walletPassword;
+    }
 }
