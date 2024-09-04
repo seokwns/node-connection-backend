93703f2acdaadb04098e8dbe258d0efc11332fbb
seokwns <seokjun0915@icloud.com>
2024-09-04 10:14:03 +0900

    feat: add swagger module & refactor: api path
    - 테스트를 위한 swagger 추가
    - api 주소 수정

--- node-connection-backend/src/main/java/node/connection/controller/WalletController.java
+++ node-connection-backend/src/main/java/node/connection/controller/WalletController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/wallet")
+@RequestMapping("/api/wallet")
 public class WalletController {
     private final WalletService walletService;
 
