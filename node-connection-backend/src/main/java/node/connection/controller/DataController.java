05a03d0f5e0db9dcce9a0b8ae3faef259837be9a
seokwns <seokjun0915@icloud.com>
2024-09-23 22:31:11 +0900

    feat: add user details for permission
    - 권한을 위해 user detail 추가

--- node-connection-backend/src/main/java/node/connection/controller/DataController.java
+++ node-connection-backend/src/main/java/node/connection/controller/DataController.java
@@ -1,9 +1,12 @@
 package node.connection.controller;
 
 import node.connection._core.response.Response;
+import node.connection._core.security.CustomUserDetails;
 import node.connection.service.ConfigDataService;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,14 +21,18 @@ public class DataController {
     }
 
     @PatchMapping("/chain-code/court")
-    public ResponseEntity<?> updateCourtChainCode(@RequestBody String version) {
-        this.configDataService.updateCourtChainCodeVersion(version);
+    public ResponseEntity<?> updateCourtChainCode(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                  @RequestBody String version
+    ) {
+        this.configDataService.updateCourtChainCodeVersion(userDetails, version);
         return ResponseEntity.ok().body(Response.success(null));
     }
 
     @PostMapping("/chain-code/registry")
-    public ResponseEntity<?> updateRegistryChainCode(@RequestBody String version) {
-        this.configDataService.updateRegistryChainCodeVersion(version);
+    public ResponseEntity<?> updateRegistryChainCode(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                     @RequestBody String version
+    ) {
+        this.configDataService.updateRegistryChainCodeVersion(customUserDetails, version);
         return ResponseEntity.ok().body(Response.success(null));
     }
 }
