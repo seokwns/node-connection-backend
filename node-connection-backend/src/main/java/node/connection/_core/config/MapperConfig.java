65a9220901b5cfed7692c3ef633533444eb01834
seokwns <seokjun0915@icloud.com>
2024-10-09 11:56:11 +0900

    config: add time deserializer
    - 하이퍼레저 패브릭 응답의 시간 필드 파싱을 위힌 deserializer 추가

--- node-connection-backend/src/main/java/node/connection/_core/config/MapperConfig.java
+++ node-connection-backend/src/main/java/node/connection/_core/config/MapperConfig.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import node.connection.hyperledger.fabric.ca.CAEnrollment;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class MapperConfig {
@@ -25,6 +29,9 @@ public class MapperConfig {
     public ObjectMapper objectMapper(@Qualifier("CustomModule") SimpleModule simpleModule) {
         ObjectMapper objectMapper = new ObjectMapper();
         JavaTimeModule timeModule = new JavaTimeModule();
+
+        timeModule.addDeserializer(LocalDateTime.class,
+                new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.registerModule(timeModule);
